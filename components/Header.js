5f6c08fc971d1a354ae2a504caea27ff6cc7427f
Kareem Arab <me@kareemarab.com>
2020-05-14 11:12:26 -0400

    added about page

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import Link from 'next/link';
 import PropTypes from 'prop-types';
 
 const Header = ({ t }) => (
     <div className="header">
         <p>im a header</p>
+        <ul>
+            <li>
+                <Link href='/about'>
+                    about
+                </Link>
+            </li>
+            <li><a href="#">writing</a></li>
+            <li><a href="#">projects</a></li>
+            <li><a href="#">bookshelf</a></li>
+            <li><a href="#">research</a></li>
+            <li><a href="#">film</a></li>
+            <li><a href="#">mildly interesting</a></li>
+            <li><a href="#">music</a></li>
+            <li><a href="#">resume</a></li>
+        </ul>
     </div>
 );
 
--- pages/index.js
+++ pages/index.js
@@ -5,17 +5,6 @@ export default function IndexPage() {
   return (
     <Layout>
       <h2>kareem's apprehension</h2>
-      <ul>
-        <li><a href="#">about</a></li>
-        <li><a href="#">writing</a></li>
-        <li><a href="#">projects</a></li>
-        <li><a href="#">bookshelf</a></li>
-        <li><a href="#">research</a></li>
-        <li><a href="#">film</a></li>
-        <li><a href="#">mildly interesting</a></li>
-        <li><a href="#">music</a></li>
-        <li><a href="#">resume</a></li>
-      </ul>
     </Layout>
   )
 }
