f7f04db60cebcffaaa46db4006ea214a3bf0a36d
Kareem Arab <kareem3043@gmail.com>
2020-07-26 23:04:40 -0400

    updates

--- components/Header.js
+++ components/Header.js
@@ -4,45 +4,20 @@ import config from '../data/config';
 
 export default class Header extends Component {
 
-    state = {
-        scrolled: false,
-    }
-
-    componentDidMount() {
-        window.addEventListener('scroll', this.navOnScroll)
-    }
-
-    componentWillUnmount() {
-        window.removeEventListener('scroll', this.navOnScroll)
-    }
-
-    navOnScroll = () => {
-        if (window.scrollY > 20) {
-            this.setState({ scrolled: true })
-        } else {
-            this.setState({ scrolled: false})
-        }
-    }
-
     render() {
-        const { scrolled } = this.state
-
         return (
-            <nav className={scrolled ? 'nav scroll' : 'nav'}>
-                <nav className='nav-container'>
-                    <div className='logo'>
-                        <Link href='/'>
-                            <span className="text">Kareem's apprehension</span>
-                        </Link>
-                    </div>
-                    <div className="links">
+            <nav className='nav-container'>
+                <div className="links-container">
+                    <ul>
                         {config.menuLinks.map(link => (
-                            <Link href={link.link}>
-                                {link.name}
-                            </Link>
+                            <li>
+                                <Link href={link.link}>
+                                    {link.name}
+                                </Link>
+                            </li>
                         ))}
-                    </div>
-                </nav>
+                    </ul>
+                </div>
             </nav>
         )
     }
